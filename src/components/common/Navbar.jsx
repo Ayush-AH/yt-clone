810f2ba41e8aa6cc65664118e025350c1b7b24ae
Ayush Ahirwar <ayushahirwar6673@gmail.com>
2025-06-09 13:59:37 +0530

    update

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
     if (input.trim().length === 0) return;
 
     navigate(`/?search_query=${input}`);
-    dispatch(setCategory(query));
+    dispatch(setCategory(input));
   };
 // useEffect(() => {
 //   const fetchSuggestions = async () => {
